d95bd7910ffa011823dba3477fdb297403adaf25
임동현 <79711744+ldh3907@users.noreply.github.com>
2021-08-22 20:27:16 +0900

    setting : files, update : App
    템플릿 문자열, 배열 전개 연산자, 객체 전개 연산자  예제 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// var string1 = "안녕하세요.";
+// var string2 = "반갑습니다.";
+// var greeting = `${string1 + string2}`;
+// var product = { name: "도서", price: "4200원" };
+// var message = `제품 ${product.name}의 가격은 ${product.price}입니다.`;
+// 템플릿 문자열 예제
+
+// var array1 = ["one", "two"];
+// var array2 = ["three", "four"];
+// const combined = [...array1, ...array2];
+// const [first, second, three = "empty", ...others] = array1;
+// function func(...args) {
+//   var [first, ...others] = args;
+// }
+// 배열 전개 연산자 예제
+
+// var objectOne = { one: 1, two: 2, other: 0 };
+// var objecttwo = { three: 3, four: 4, other: -1 };
+// var combined1 = {
+//   ...objectOne,
+//   ...objecttwo,
+// };
+// var combined2 = {
+//   ...objecttwo,
+//   ...objectOne,
+// };
+// var { other, ...others } = combined2;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="title">두잇! 리액트 시작하기</h1>
+        <div></div>
+      </div>
+    );
+  }
 }
 
 export default App;
